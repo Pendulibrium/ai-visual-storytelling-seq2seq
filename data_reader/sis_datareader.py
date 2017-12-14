6b3b66b6f7fd54031d687fb8f734913d103b6e22
Marko Smilevski <m.smilevski57@gmail.com>
2017-12-14 11:03:25 +0100

    Added new functions to sis_datareader

--- data_reader/sis_datareader.py
+++ data_reader/sis_datareader.py
@@ -84,22 +84,60 @@ class SIS_DataReader:
         print(save_sent)
         return min_sentence_length
 
-    def sentences_to_index(self,vocabulary_file='../dataset/vist2017_vocabulary.json'):
+    def sentences_to_index(self, vocabulary_file='../dataset/vist2017_vocabulary.json'):
         #treba da se dopolni koga kje znaeme sto da pravime so maksimumot
-        vocabulary=json.load(open(vocabulary_file))
-        words_to_idx=vocabulary["words_to_idx"]
+        vocabulary = json.load(open(vocabulary_file))
+        words_to_idx = vocabulary["words_to_idx"]
 
-        data=json.load(open(self.path_to_file))
-        annotations=data["annotations"]
+        data = json.load(open(self.path_to_file))
+        annotations = data["annotations"]
+        #print(annotations[0][0].keys())
+        total_length_mean=0.0
+        stories_to_id = {}
+        for i in range(0, len(annotations),5):
+            story_id = annotations[i][0]["story_id"]
+            story1, length1 = self.sentences_to_index_helper(annotations[i][0]["text"],words_to_idx)
+            story2, length2 = self.sentences_to_index_helper(annotations[i+1][0]["text"], words_to_idx)
+            story3, length3 = self.sentences_to_index_helper(annotations[i+2][0]["text"], words_to_idx)
+            story4, length4 = self.sentences_to_index_helper(annotations[i+3][0]["text"], words_to_idx)
+            story5, length5 = self.sentences_to_index_helper(annotations[i+4][0]["text"], words_to_idx)
+            stories_to_id[story_id] = [story1, story2, story3, story4, story5]
+            total_length_mean += np.mean([length1, length2, length3, length4, length5])
+            print(np.mean([length1, length2, length3, length4, length5]))
+            #print(annotations[i][0]["worker_arranged_photo_order"],annotations[i+1][0]["worker_arranged_photo_order"],annotations[i+2][0]["worker_arranged_photo_order"],      annotations[i+3][0]["worker_arranged_photo_order"],annotations[i+4][0]["worker_arranged_photo_order"])
 
-        for i in range(len(annotations)):
-            print(annotations[i][0]["story_id"])
+        print(total_length_mean/float(len(annotations)/5.0))
+
+        with open("../dataset/stories_to_id.json", 'w') as fp:
+            json.dump(stories_to_id, fp)
 
 
+    #da se dopolni za koga kje znaeme kolku kje bide max-length
+    def sentences_to_index_helper(self,sentence,word_to_idx):
+        sentence="<START> "+sentence+" <END>"
+        words=sentence.split()
+        result_sentence=[]
+        for word in words:
+            if(word_to_idx.has_key(word)):
+                result_sentence.append(word_to_idx[word])
+            else:
+                result_sentence.append(word_to_idx["<UNK>"])
+
+        return result_sentence, len(result_sentence)
+
+    def indecies_to_sentence(self, sentence, idx_to_word):
+        result_sentence=""
+        for word in sentence:
+            result_sentence=result_sentence+" "+idx_to_word[word]
+
+        print(result_sentence)
+        return result_sentence
+
+    
+# object=SIS_DataReader()
+# object.sentences_to_index()
 
 
-object=SIS_DataReader()
-object.sentences_to_index()
 
 # data = json.load(open('../dataset/word_frequencies.json'))
 # print(type(data))
