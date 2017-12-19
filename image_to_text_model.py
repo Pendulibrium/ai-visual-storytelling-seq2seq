b60001d420c75c6f597bbb5d23ea85a23664fd41
wf-admin <m.smilevski57@gmail.com>
2017-12-19 15:55:10 +0100

    Define and refactor hyper parameters

--- image_to_text_model.py
+++ image_to_text_model.py
@@ -51,45 +51,30 @@ def generate_input_from_file(file_path, vocabulary_path, batch_size):
                     dtype='float32')
 
 
-# process = psutil.Process(os.getpid())
-# print(process.memory_info().rss / 1024 / 1024)
-#
 vocab_json = json.load(open('./dataset/vist2017_vocabulary.json'))
-# train_file = h5py.File('./dataset/image_embeddings_to_sentence/stories_to_index_train.hdf5','r')
-#
-#
+train_file = h5py.File('./dataset/image_embeddings_to_sentence/stories_to_index_train.hdf5','r')
+
 batch_size = 13  # Batch size for training.
-epochs = 1  # Number of epochs to train for.
+epochs = 2  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
-# num_samples = len(train_file["story_ids"])
-#
+word_embedding_size = 300
+
+num_samples = len(train_file["story_ids"])
 num_decoder_tokens = len(vocab_json['idx_to_words'])
-#
-# max_encoder_seq_length = train_file['image_embeddings'].shape[1]
-# max_decoder_seq_length = train_file['story_sentences'].shape[1] * train_file['story_sentences'].shape[2]
-#
-# print('Number of samples:', train_file['image_embeddings'].shape[0])
-# print('Max sequence length for inputs:', max_encoder_seq_length)
-# print('Max sequence length for outputs:', max_decoder_seq_length)
+
+
 # print('Vocab size: ', num_decoder_tokens)
-#
-#
-# print("After image embedding", process.memory_info().rss / 1024 / 1024)
-#
-# # encoder_input_data = image_embeddings2
-# # print(encoder_input_data.shape)
-#
-#
+
 # # Shape (num_samples, 4096), 4096 is the image embedding length
 encoder_inputs = Input(shape=(None, 4096))
-# print(encoder_inputs.shape)
+
 encoder = LSTM(latent_dim, return_state=True)
 encoder_outputs, state_h, state_c = encoder(encoder_inputs)
 encoder_states = [state_h, state_c]
 
 decoder_inputs = Input(shape=(22,))
 
-embedding_layer = Embedding(num_decoder_tokens, 300, mask_zero=True)
+embedding_layer = Embedding(num_decoder_tokens, word_embedding_size, mask_zero=True)
 embedding_outputs = embedding_layer(decoder_inputs)
 decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
 decoder_outputs, _, _ = decoder_lstm(embedding_outputs, initial_state=encoder_states)
@@ -102,4 +87,4 @@ model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
 
 model.fit_generator(generate_input_from_file('./dataset/image_embeddings_to_sentence/stories_to_index_train.hdf5',
                                              './dataset/vist2017_vocabulary.json', batch_size),
-                    samples_per_epoch = 40115 / batch_size, epochs = 1)
+                    samples_per_epoch = num_samples / batch_size, epochs = epochs)
