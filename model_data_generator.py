782bb1f07f4a8779cd173aeae23bd03604dbd34c
Ilija <ilija@webfactory.mk>
2018-01-12 11:46:25 +0100

    Implemented image caption model generator

--- model_data_generator.py
+++ model_data_generator.py
@@ -5,7 +5,6 @@ from nlp import nlp
 
 
 class ModelDataGenerator:
-
     def __init__(self, dataset, vocab_json, batch_size, num_samples_per_epoch=None):
 
         self.dataset = dataset
@@ -77,27 +76,69 @@ class ModelDataGenerator:
                     decoder_batch_input_data.fill(0)
                     decoder_batch_target_data.fill(0)
 
+    '''
+        Generate only one sample per story, all images included in the sample.
+    '''
 
-'''
-    Generate only one sample per story, all images included in the sample.
-'''
+    def one_sample_from_story_generator(self, reverse=False, concatenate_all_sentences=False):
+        print("one sample from story")
+
+    '''
+        Generate multiple samples from one story with only one image, mapping the image to the descirption of the image
+    '''
+
+    def image_caption_generator(self):
+        while 1:
+
+            story_batch_size = int(np.round(self.batch_size / float(self.story_length)))  # Number of stories
+            approximate_batch_size = story_batch_size * self.story_length  # Actual batch size
+
+            encoder_batch_input_data = np.zeros((approximate_batch_size, 1, self.image_embeddings_size))
+            decoder_batch_input_data = np.zeros((approximate_batch_size, self.sentences_length), dtype=np.int32)
+            decoder_batch_target_data = np.zeros((approximate_batch_size, self.sentences_length, self.number_of_tokens),
+                                                 dtype=np.int32)
+            for i in range(self.num_samples):
+                for j in range(self.story_length):
+                    encoder_row_start_range = ((i % story_batch_size) * self.story_length) + j
+
+                    encoder_batch_input_data[encoder_row_start_range, 0] = self.image_embeddings[i][j]
+
+                    decoder_row = (i % story_batch_size) * self.story_length + j
+
+                    temp_story = self.story_sentences[i][j].tolist()
+                    end_index = temp_story.index(2)
+                    temp_story[end_index] = 0
+                    decoder_batch_input_data[decoder_row] = np.array(temp_story)
+
+                story = self.story_sentences[i]
+                for sentence_index in range(len(story)):
+                    sentence = story[sentence_index]
+                    for word_index in range(len(sentence)):
+                        if word_index > 0:
+                            decoder_row = ((i % story_batch_size) * self.story_length) + sentence_index
+                            decoder_batch_target_data[decoder_row, word_index - 1, sentence[word_index]] = 1
+
+                if ((i + 1) % story_batch_size) == 0 and i != 0:
+                    yield ([encoder_batch_input_data, decoder_batch_input_data], decoder_batch_target_data)
+
+                    encoder_batch_input_data.fill(0.0)
+                    decoder_batch_input_data.fill(0)
+                    decoder_batch_target_data.fill(0)
+
+# vocab_json = json.load(open('./dataset/vist2017_vocabulary.json'))
+# train_dataset = h5py.File('./dataset/image_embeddings_to_sentence/stories_to_index_train.hdf5', 'r')
+# model_generator = ModelDataGenerator(train_dataset, vocab_json, 64)
+# generator = model_generator.image_caption_generator()
+
+# batch_data = generator.next()
 
 
-def one_sample_from_story_generator(self, reverse=False, concatenate_all_sentences=False):
-    print("one sample from story")
 
-
-'''
-    Generate multiple samples from one story with only one image, mapping the image to the descirption of the image
-'''
-
-
-def image_caption_generator(self):
-    print("image caption generator")
-
-
-vocab_json = json.load(open('./dataset/vist2017_vocabulary.json'))
-train_dataset = h5py.File('./dataset/image_embeddings_to_sentence/stories_to_index_train.hdf5', 'r')
-model_generator = ModelDataGenerator(train_dataset, vocab_json, 64)
-generator = model_generator.multiple_samples_per_story_generator()
-
+# image_embeddings = batch_data[0][0]
+# decoder_input = batch_data[0][1]
+# decoder_output = batch_data[1]
+#
+# image_embeddings_dataset = train_dataset['image_embeddings']
+#
+# for i in range(len(image_embeddings)):
+#     print(np.array_equal(image_embeddings[i][0], image_embeddings_dataset[i / 5 + 14][i % 5]))
