007b3279ada5d701c959c8508d85c52cb62c597d
Ilija <ilija@webfactory.mk>
2018-01-25 00:14:25 +0100

    Implemented BLEU corpus scoring

--- model_scores.py
+++ model_scores.py
@@ -1,14 +1,32 @@
 import commands
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
+
 
 # Meteor scoring
-hypotheses_filename = "./results/hypotheses_2018-01-20_22:10:16-2018-01-21_09:53:21_valid.txt"
-references_filename = "./results/original_valid.txt"
+hypotheses_filename = "./results/hypotheses_2018-01-18_17:39:24-2018-01-20_18:50:39_train.txt"
+references_filename = "./results/original_train.txt"
+
 status, output = commands.getstatusoutput(
     "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -l en -norm")
 
-text_file = open("./results/meteor_2018-01-20_22:10:16-2018-01-21_09:53:21_valid.txt", "w")
+text_file = open("./results/meteor_2018-01-18_17:39:24-2018-01-20_18:50:39_train.txt", "w")
 text_file.write(output)
 text_file.close()
 
 #Bleu scoring
-#Implemented scoring with nltk corpus method
+
+with open(references_filename) as f:
+    references = f.readlines()
+references = [x.strip() for x in references]
+references = [[x.split()] for x in references]
+
+with open(hypotheses_filename) as f:
+    hypotheses = f.readlines()
+hypotheses = [x.strip().split() for x in hypotheses]
+
+
+print(corpus_bleu(references, hypotheses) * 100)
+
+
+
+
