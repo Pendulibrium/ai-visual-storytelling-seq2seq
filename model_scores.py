1e5b68a392a6209ff2dd8c31f87e191dd0dd2da4
Ilija <ilija@webfactory.mk>
2018-01-24 13:39:14 +0100

    Rename files

--- model_scores.py
+++ model_scores.py
@@ -1,13 +1,14 @@
 import commands
 
 # Meteor scoring
-hypotheses_filename = "./results/hypotheses_2018-01-18_17:39:24-2018-01-20_18:50:39_valid.txt"
+hypotheses_filename = "./results/hypotheses_2018-01-20_22:10:16-2018-01-21_09:53:21_valid.txt"
 references_filename = "./results/original_valid.txt"
 status, output = commands.getstatusoutput(
     "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -l en -norm")
 
-text_file = open("./results/meteor_2018-01-18_17:39:24-2018-01-20_18:50:39_valid.txt", "w")
+text_file = open("./results/meteor_2018-01-20_22:10:16-2018-01-21_09:53:21_valid.txt", "w")
 text_file.write(output)
 text_file.close()
 
 #Bleu scoring
+#Implemented scoring with nltk corpus method
