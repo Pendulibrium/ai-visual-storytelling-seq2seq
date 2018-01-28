6a9e323d4f99b0795747b0fa630b832f3d86f1d6
Ilija <ilija@webfactory.mk>
2018-01-28 22:19:58 +0100

    Added scores for 3 layered model

--- model_scores.py
+++ model_scores.py
@@ -3,13 +3,13 @@ from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 
 
 # Meteor scoring
-hypotheses_filename = "./results/hypotheses_2018-01-18_17:39:24-2018-01-20_18:50:39_train.txt"
-references_filename = "./results/original_train.txt"
+hypotheses_filename = "./results/2018-01-25_10:05:24-2018-01-27_22:34:08/hypotheses_valid.txt"
+references_filename = "./results/original_valid.txt"
 
 status, output = commands.getstatusoutput(
     "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -l en -norm")
 
-text_file = open("./results/meteor_2018-01-18_17:39:24-2018-01-20_18:50:39_train.txt", "w")
+text_file = open("./results/2018-01-25_10:05:24-2018-01-27_22:34:08/meteor_valid", "w")
 text_file.write(output)
 text_file.close()
 
@@ -25,7 +25,7 @@ with open(hypotheses_filename) as f:
 hypotheses = [x.strip().split() for x in hypotheses]
 
 
-print(corpus_bleu(references, hypotheses) * 100)
+print("Bleu score", corpus_bleu(references, hypotheses) * 100)
 
 
 
