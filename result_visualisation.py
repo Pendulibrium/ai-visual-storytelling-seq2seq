20099849dc8428417f57825de6705e810a7523cd
Marko Smilevski <m.smilevski57@gmail.com>
2018-02-02 17:26:22 +0100

    Small performance improvement for beam search method, implemented batch calculation for the image embeddings

--- result_visualisation.py
+++ result_visualisation.py
@@ -70,7 +70,75 @@ class Inference:
             for i in range(beam_size):
 
                 state_value_tmp = [state_value]
-                for i in range(self.num_stacked_layers - 1):
+                for k in range(self.num_stacked_layers - 1):
+                    state_value_tmp.append(np.zeros(state_value_shape))
+
+                state_values.append(state_value_tmp)
+
+            j = 0
+
+            while live_beam and dead_beam < beam_size:
+
+                probs, new_states = self.beam_search_predict_helper(live_sentence, state_values)
+
+                if j == 0:
+                    state_values = []
+                    for _ in range(beam_size):
+                        state_values.append(new_states[0])
+
+                else:
+                    state_values = new_states
+
+                j += 1
+                cand_scores = np.array(live_score)[:, None] - np.log(probs)
+                cand_flat = cand_scores.flatten()
+
+                ranks_flat = cand_flat.argsort()[:(beam_size - dead_beam)]
+                live_score = cand_flat[ranks_flat]
+
+                live_sentence = [live_sentence[r // self.vocab_size] + [r % self.vocab_size] for r in ranks_flat]
+
+                zombie = [s[-1] == 2 or len(s) >= max_decoder_seq_length for s in live_sentence]
+
+                # add zombies to the dead
+                dead_sentence += [s for s, z in zip(live_sentence, zombie) if z]  # remove first label == empty
+                dead_scores += [s for s, z in zip(live_score, zombie) if z]
+                dead_beam = len(dead_sentence)
+                # remove zombies from the living
+                live_sentence = [s for s, z in zip(live_sentence, zombie) if not z]
+                live_score = [s for s, z in zip(live_score, zombie) if not z]
+                state_values = [s for s, z in zip(state_values, zombie) if not z]
+
+                live_beam = len(live_sentence)
+
+            decoded_sentences.append(dead_sentence + live_sentence)
+            scores.append(dead_scores + live_score)
+
+        # returns sentence in integer list, should fix this
+        return decoded_sentences, scores
+
+    def predict_story_beam_search1(self, input_sequence, max_decoder_seq_length=22, images_per_story=5,
+                                   image_embed_size=4096, beam_size=3):
+
+        encoder_states_value = self.encoder_model.predict(input_sequence)
+        decoded_sentences = []
+        scores = []
+        m = 1
+        for state_value in encoder_states_value:
+            state_value = np.reshape(state_value, (1, state_value.shape[0]))
+            state_value_shape = state_value.shape
+            live_beam = 1
+            live_sentence = [[self.words_to_idx["<START>"]]]
+            live_score = [0]
+            dead_beam = 0
+            dead_sentence = []
+            dead_scores = []
+            state_values = []
+
+            for i in range(beam_size):
+
+                state_value_tmp = [state_value]
+                for k in range(self.num_stacked_layers - 1):
                     state_value_tmp.append(np.zeros(state_value_shape))
 
                 state_values.append(state_value_tmp)
@@ -186,7 +254,8 @@ class Inference:
         hypotheses_file.writelines(hypotheses_sentences_with_new_line)
         hypotheses_file.close()
 
-    def predict_all_beam_search(self, batch_size, beam_size=3, sentence_length=22, references_file_name='', hypotheses_file_name=''):
+    def predict_all_beam_search(self, batch_size, beam_size=3, sentence_length=22, references_file_name='',
+                                hypotheses_file_name=''):
 
         data_generator = ModelDataGenerator(self.dataset_file, self.vocab_json, batch_size)
         count = 0
@@ -199,13 +268,13 @@ class Inference:
             original_sentences_input = batch[0][1]
             print(encoder_batch_input_data.shape)
             # encoder_batch_input_data = encoder_batch_input_data[0:1,]
-            decoded = self.predict_story_beam_search(encoder_batch_input_data, beam_size=beam_size)
-            for i in range(len(decoded[0])):
 
+            # TODO predict_story_beam_search1 function is only a mock function, further tests are needed for better performance
+            decoded = self.predict_story_beam_search1(encoder_batch_input_data, beam_size=beam_size)
+            for i in range(len(decoded[0])):
                 max_score_index = np.argmin(decoded[1][i])
                 #print("Decoded",nlp.vec_to_sentence(decoded[0][i][max_score_index], self.idx_to_words))
                 hypotheses.append(nlp.vec_to_sentence(decoded[0][i][max_score_index], self.idx_to_words))
-
 
         if references_file_name:
             original_file = open("./results/" + references_file_name, "w")
