77dec2298eb766b06d6ab3397b0e8d3e5f518b4d
Marko Smilevski <m.smilevski57@gmail.com>
2018-02-02 11:03:21 +0100

    Changed code so we can evaluate beam search results

--- model_scores.py
+++ model_scores.py
@@ -4,19 +4,31 @@ from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 # Calculating Meteor score
 dataset_type = 'valid'
 results_model_dir = "./results/2018-01-29_00:37:26-2018-01-31_00:01:42/"
-hypotheses_filename = results_model_dir + "hypotheses_" + dataset_type + ".txt"
+beam_size = 3
+if beam_size > 1:
+    hypotheses_filename = results_model_dir + "hypotheses_" + dataset_type + "_beam" + str(beam_size) + ".txt"
+else:
+    hypotheses_filename = results_model_dir + "hypotheses_" + dataset_type + ".txt"
+
 references_filename = "./results/original_" + dataset_type + ".txt"
 
 status, output_meteor = commands.getstatusoutput(
     "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -l en -norm")
 
-text_file = open(results_model_dir + "meteor_" + dataset_type, "w")
+if beam_size > 1:
+    text_file = open(results_model_dir + "meteor_" + dataset_type + "_beam" + str(beam_size), "w")
+else:
+    text_file = open(results_model_dir + "meteor_" + dataset_type, "w")
 text_file.write(output_meteor)
 text_file.close()
 
 # Calculating BLEU score
 status, output_bleu = commands.getstatusoutput(
     "perl ./nlp/multi-bleu.perl " + references_filename + " < " + hypotheses_filename)
-text_file = open(results_model_dir + "bleu_" + dataset_type, "w")
+
+if beam_size > 1:
+    text_file = open(results_model_dir + "bleu_" + dataset_type + "_beam" + str(beam_size), "w")
+else:
+    text_file = open(results_model_dir + "bleu_" + dataset_type, "w")
 text_file.write(output_bleu)
 text_file.close()
