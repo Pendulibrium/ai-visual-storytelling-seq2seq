811f594d1b3892bb0a1440ae657ab4fcaa5c4cce
wf-admin <m.smilevski57@gmail.com>
2018-02-10 12:43:20 +0100

    Added model results

--- dataset/story_vis.py
+++ dataset/story_vis.py
@@ -17,7 +17,7 @@ def show_story(story_index):
 
     train_dataset = h5py.File('./image_embeddings_to_sentence/stories_to_index_valid.hdf5', 'r')
     story_ids = train_dataset['story_ids']
-    hypothesis = open('../results/2018-02-07_13:55:01-2018-02-08_03:15:08/hypotheses_valid.txt').read().split('\n')
+    hypothesis = open('../results/2018-02-09_15:30:08-2018-02-10_01:04:10/hypotheses_valid.txt').read().split('\n')
 
     story_id = story_ids[story_index]
     hypotheses_sentences = hypothesis[(story_index * 5): (story_index * 5) + 5]
