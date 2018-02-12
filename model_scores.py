cf1e01d5c0177ba5726ad404296ff6a6274a29ad
Marko Smilevski <m.smilevski57@gmail.com>
2018-02-12 12:21:55 +0100

    Added a function for generating a story from the hypoteses and uploaded new files for testing on the machine

--- model_scores.py
+++ model_scores.py
@@ -8,9 +8,9 @@ beam_size = 1
 if beam_size > 1:
     hypotheses_filename = results_model_dir + "hypotheses_" + dataset_type + "_beam" + str(beam_size) + ".txt"
 else:
-    hypotheses_filename = results_model_dir + "hypotheses_" + dataset_type + ".txt"
+    hypotheses_filename = results_model_dir + "hypotheses_story_" + dataset_type + ".txt"
 
-references_filename = "./results/original_" + dataset_type + ".txt"
+references_filename = "./results/original_story_" + dataset_type + ".txt"
 
 status, output_meteor = commands.getstatusoutput(
     "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -t hter -l en -norm")
@@ -18,7 +18,7 @@ status, output_meteor = commands.getstatusoutput(
 if beam_size > 1:
     text_file = open(results_model_dir + "meteor_" + dataset_type + "_beam" + str(beam_size), "w")
 else:
-    text_file = open(results_model_dir + "meteor_" + dataset_type, "w")
+    text_file = open(results_model_dir + "meteor_story_" + dataset_type, "w")
 text_file.write(output_meteor)
 text_file.close()
 
@@ -29,6 +29,6 @@ status, output_bleu = commands.getstatusoutput(
 if beam_size > 1:
     text_file = open(results_model_dir + "bleu_" + dataset_type + "_beam" + str(beam_size), "w")
 else:
-    text_file = open(results_model_dir + "bleu_" + dataset_type, "w")
+    text_file = open(results_model_dir + "bleu_story_" + dataset_type, "w")
 text_file.write(output_bleu)
 text_file.close()
--- util/util.py
+++ util/util.py
@@ -2,3 +2,23 @@ def seconds_to_formatted_string(seconds):
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     return "%d:%02d:%02dh" % (h, m, s)
+
+
+def sentences_to_story(open_file_path, save_file_path, num_sentences=5):
+    with open(open_file_path) as fp:
+        i = 1
+        stories = []
+        story = ""
+        for line in fp:
+            line = line.replace("\n", "")
+            story = story + line + " "
+            if i % 5 == 0:
+                stories.append(story)
+                story = ""
+            i += 1
+        # print stories
+
+        stories_file = open(save_file_path, "w")
+        stories_with_new_line = map(lambda x: x + "\n", stories)
+        stories_file.writelines(stories_with_new_line)
+        stories_file.close()
