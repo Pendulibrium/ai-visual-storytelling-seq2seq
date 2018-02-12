d3e790afa65155b3b8d3c05f7ea4d95ecf61698d
Marko Smilevski <m.smilevski57@gmail.com>
2018-02-12 16:02:45 +0100

    Updated model_scores so it calculates meteor and bleu score on stories, not sentences

--- model_scores.py
+++ model_scores.py
@@ -1,24 +1,24 @@
 import commands
+from util import util
+
 
 # Calculating Meteor score
 dataset_type = 'valid'
 results_model_dir = "./results/2018-02-09_15:30:08-2018-02-10_01:04:10/"
-beam_size = 1
 
-if beam_size > 1:
-    hypotheses_filename = results_model_dir + "hypotheses_" + dataset_type + "_beam" + str(beam_size) + ".txt"
-else:
-    hypotheses_filename = results_model_dir + "hypotheses_story_" + dataset_type + ".txt"
+hypotheses_sentences_filename = results_model_dir + "hypotheses_" + dataset_type + ".txt"
+hypotheses_filename = results_model_dir + "hypotheses_story_" + dataset_type + ".txt"
+
+util.sentences_to_story(hypotheses_sentences_filename, hypotheses_filename)
 
 references_filename = "./results/original_story_" + dataset_type + ".txt"
 
+# Calculating METEOR
 status, output_meteor = commands.getstatusoutput(
     "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -t hter -l en -norm")
 
-if beam_size > 1:
-    text_file = open(results_model_dir + "meteor_" + dataset_type + "_beam" + str(beam_size), "w")
-else:
-    text_file = open(results_model_dir + "meteor_story_" + dataset_type, "w")
+
+text_file = open(results_model_dir + "meteor_story_" + dataset_type, "w")
 text_file.write(output_meteor)
 text_file.close()
 
@@ -26,9 +26,7 @@ text_file.close()
 status, output_bleu = commands.getstatusoutput(
     "perl ./nlp/multi-bleu.perl " + references_filename + " < " + hypotheses_filename)
 
-if beam_size > 1:
-    text_file = open(results_model_dir + "bleu_" + dataset_type + "_beam" + str(beam_size), "w")
-else:
-    text_file = open(results_model_dir + "bleu_story_" + dataset_type, "w")
+
+text_file = open(results_model_dir + "bleu_story_" + dataset_type, "w")
 text_file.write(output_bleu)
 text_file.close()
