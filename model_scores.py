bb8a114fa85364d50bf6331dd03246ac1f82d1a8
wf-admin <m.smilevski57@gmail.com>
2018-06-19 10:58:08 +0200

    Update model_scores.py so it can generate a match matrix

--- model_scores.py
+++ model_scores.py
@@ -1,10 +1,10 @@
 import commands
+import subprocess
 from util import util
-
 
 # Calculating Meteor score
 dataset_type = 'valid'
-results_model_dir = "./results/2018-02-09_15:30:08-2018-02-10_01:04:10/"
+results_model_dir = "./results/2018-05-10_09:56:42-2018-05-10_18:08:17/"
 
 hypotheses_sentences_filename = results_model_dir + "hypotheses_" + dataset_type + ".txt"
 hypotheses_filename = results_model_dir + "hypotheses_story_" + dataset_type + ".txt"
@@ -15,8 +15,7 @@ references_filename = "./results/original_story_" + dataset_type + ".txt"
 
 # Calculating METEOR
 status, output_meteor = commands.getstatusoutput(
-    "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -t hter -l en -norm")
-
+    "java -Xmx2G -jar nlp/meteor-1.5.jar " + hypotheses_filename + " " + references_filename + " -t hter -l en -norm -writeAlignments -f system1")
 
 text_file = open(results_model_dir + "meteor_story_" + dataset_type, "w")
 text_file.write(output_meteor)
@@ -26,7 +25,8 @@ text_file.close()
 status, output_bleu = commands.getstatusoutput(
     "perl ./nlp/multi-bleu.perl " + references_filename + " < " + hypotheses_filename)
 
-
 text_file = open(results_model_dir + "bleu_story_" + dataset_type, "w")
 text_file.write(output_bleu)
 text_file.close()
+
+commands.getstatusoutput("python meteor-1.5/xray/xray.py -p sistem1 ai-visualstorytelling/system1-align.out")
